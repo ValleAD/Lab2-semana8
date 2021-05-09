207ff369393509f9491bac4dc4ad1efb3a7e9759
Ángel Daniel Valle Pineda <angel.valle1208@gmail.com>
2021-05-08 18:32:06 -0600

    Clase Read

--- src/Registro/Read.java
+++ src/Registro/Read.java
@@ -5,6 +5,23 @@ import java.sql.SQLException;
 
 public class Read {
     public Read() throws SQLException{
+        System.out.println("<<  CONSULTA DE REGISTROS   >>");
+        mostrarResultados();
+    }
+    private void mostrarResultados() throws SQLException{
+        try{
+            ConexionCRUD utilerias = new ConexionCRUD();
+            String tabla = "estudiante";
+            String camposTabla = "*";
+            
+            String condicionBusqueda = "";
+            
+            utilerias.desplegarregistro(tabla, camposTabla, condicionBusqueda);
+        } catch (SQLException ex){
+            System.out.println("Ha ocurrido el siguiente error: " + ex.getMessage());
+        } finally{
+            Menu_Principal.desplegarMenu();
+        }
         
     }
 }
