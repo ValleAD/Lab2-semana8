48b9e6356edd9bf9ceeadc4149389e7a28b197df
carlosUD <edudelcid2001@gmail.com>
2021-05-09 17:18:13 -0600

    base de datos

--- recurso/Script Base de datos/BD del estudiante.sql
+++ recurso/Script Base de datos/BD del estudiante.sql
@@ -3,7 +3,7 @@ create database lab2_semana8;
  	create table estudiante(
 		id_estudiante int primary key auto_increment not null,
 		carnet_estudiante varchar(6) not null,
-        nom_estudiante  varchar(30) not null,
-        ape_estudiante  varchar(30) not null,
-        edad int(3) not null
+        	nom_estudiante  varchar(30) not null,
+        	ape_estudiante  varchar(30) not null,
+        	edad int(3) not null
 )ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1001 ;
